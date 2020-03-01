20aba7e73fca73a8a3002398048fdbdbbcf60e46
Monica Luo <lalaphoon@gmail.com>
2020-02-29 19:23:41 -0500

    Added confirmation page.

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import '../styles/Confirmation.css'
+import {Progress, Form, Button} from 'antd';
+import { withRouter } from  "react-router-dom"
 
-export function Confirmation() {
-    return (<div>Confirmation</div>);
+class ConfirmationForm extends React.Component {
+
+    goBackToHome = () =>  {
+        this.props.history.push('/user_status');
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Progress type="circle" percent={100} format={() => 'Done'} />
+                <Button onClick={this.goBackToHome}>Back to Home </Button>
+            </div>
+        );
+    }
 }
+
+export const Confirmation = withRouter(Form.create({name: "confirmation"})(ConfirmationForm));
